8b7669fc2f39782e5ba6dd1de8699fe4f48b7fab
JangHoonSeok <orirori512@naver.com>
2024-01-30 01:11:09 +0900

    🔧 config: craco 설정 변경

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,17 @@
-const solution = (play_list, listen_time) => {
-  const totalTime = play_list.reduce((acc, cur) => acc + cur, 0);
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { CracoAliasPlugin } = require('react-app-alias');
 
-  if (totalTime <= listen_time) return play_list.length;
-
-  const newPlayList = [...play_list, ...play_list];
-  let maxSongs = 0;
-
-  for (let i = 0; i < play_list.length; i++) {
-    const availableSongs = checkAvailableSongs(i, newPlayList, listen_time);
-
-    if (availableSongs > maxSongs) maxSongs = availableSongs;
-  }
-
-  return maxSongs;
+const options = {
+  source: 'tsconfig',
+  tsConfigPath: 'tsconfig.paths.json',
 };
 
-const checkAvailableSongs = (startIndex, newPlayList, listen_time) => {
-  let timeSpent = listen_time - 1;
-  let availableSongs = 1;
-  let currentIndex = startIndex + 1;
-
-  while (timeSpent > 0) {
-    timeSpent -= newPlayList[currentIndex];
-    currentIndex++;
-    availableSongs++;
-  }
-
-  return availableSongs;
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options,
+    },
+  ],
 };
