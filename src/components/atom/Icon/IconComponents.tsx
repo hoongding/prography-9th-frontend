a52b5ad257fd9de5af4694d30d71ac6d58eb425b
JangHoonSeok <orirori512@naver.com>
2024-01-31 19:08:59 +0900

    ✨ feat: x icon 추가

--- src/components/atom/Icon/IconComponents.tsx
+++ src/components/atom/Icon/IconComponents.tsx
@@ -1,7 +1,9 @@
 import { ReactComponent as plus } from '@assets/Icons/plus.svg';
+import { ReactComponent as cancel } from '@assets/Icons/cancel.svg';
 
 const IconComponents = {
   plus,
+  cancel,
 };
 
 export { IconComponents };
