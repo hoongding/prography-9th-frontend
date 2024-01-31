55f8ba6dacfc34305f1cc88808e48f1e5b3fb282
JangHoonSeok <orirori512@naver.com>
2024-01-31 22:12:48 +0900

    ✨ feat: 실제로 받아온 data를 기반으로 카테고리 버튼 생성

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,10 +1,35 @@
 import { useAllCategories } from '@apis/categories/queries';
-import React from 'react';
+import React, { useState } from 'react';
+import { Chip } from './atom/Chip';
+import { ICategory } from '@apis/categories/types';
 
 const Categories = () => {
-  const { data } = useAllCategories();
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  return <div>Categories</div>;
+  const { data, isLoading: categoriesLoading } = useAllCategories();
+
+  const handleSelectedCategory = (idCategory: string) => {
+    setSelectedCategories(prev => [...prev, idCategory]);
+  };
+
+  return categoriesLoading ? (
+    <div />
+  ) : (
+    <>
+      {data?.categories.map((category: ICategory) => {
+        return (
+          <Chip
+            key={category.idCategory}
+            onClick={() => handleSelectedCategory(category.idCategory)}
+            isSelected={selectedCategories.some(categoryId => categoryId === category.idCategory)}
+          >
+            <Chip.Label>{category.strCategory}</Chip.Label>
+            <Chip.Icon />
+          </Chip>
+        );
+      })}
+    </>
+  );
 };
 
 export default Categories;
