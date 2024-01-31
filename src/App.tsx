4b9baca048196b2e3e2e7b2854d87f81ec8a8d9c
JangHoonSeok <orirori512@naver.com>
2024-02-01 01:01:11 +0900

    ✨ feat: Header 컴포넌트 추가 및 GNB 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import theme from '@styles/theme';
 import GlobalStyles from '@styles/GlobalStyles.style';
 import Categories from '@components/Categories';
+import Header from '@components/Header';
 
 const queryClient = new QueryClient();
 
@@ -13,10 +14,20 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
-        <Categories />
+        <Container>
+          <Header />
+          <Categories />
+        </Container>
       </QueryClientProvider>
     </ThemeProvider>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0px 20px;
+`;
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from './atom/Image';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Image src="./assets/GNB.png" alt="GNB" width={120} />
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: start;
+  padding-top: 20px;
+  padding-bottom: 60px;
+`;
+
+export default Header;
