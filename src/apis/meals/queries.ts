dc75affcd4764b7d47d6dab0abc0ae9dfe262e09
JangHoonSeok <orirori512@naver.com>
2024-02-01 01:56:19 +0900

    ✨ feat: meals query hook 생성

---
+++ src/apis/meals/queries.ts
@@ -0,0 +1,20 @@
+import QueryApi from '@apis/react-query/queryApiClass';
+import apis from '@constants/apis';
+import { useQuery } from '@tanstack/react-query';
+import { IMeals } from './types';
+import { AxiosError } from 'axios';
+
+const meals = new QueryApi(apis.foods);
+
+interface IMealsSearchParams {
+  c: string;
+}
+
+export const useMeals = (category: string[]) => {
+  const searchParams: IMealsSearchParams = { c: category.join(',') };
+
+  return useQuery<IMeals, AxiosError>({
+    queryKey: meals.getQueryKey(searchParams),
+    queryFn: meals.getQueryFn(searchParams),
+  });
+};
