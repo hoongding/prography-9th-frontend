567ee6ebd14449a6194500ac42293061fb082339
JangHoonSeok <orirori512@naver.com>
2024-01-31 20:19:02 +0900

    ✨ feat: Chip 컴포넌트 Context 생성

---
+++ src/components/atom/Chip/modules/ChipStateContext.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from 'react';
+
+interface ChipContextType {
+  onClick: () => void;
+  children: React.ReactNode;
+  isSelected: boolean;
+}
+
+export const ChipContext = createContext<ChipContextType | null>(null);
+
+export const useChipState = () => {
+  const context = useContext(ChipContext);
+
+  if (!context) {
+    throw new Error('useChipState must be used within a ChipProvider');
+  }
+
+  return context;
+};
