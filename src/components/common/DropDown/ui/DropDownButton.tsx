36208a0b2b46c889d815fa0ddca30c46d3bf491d
JangHoonSeok <orirori512@naver.com>
2024-02-02 22:56:33 +0900

    ✨ feat: 버튼 내에 있는 svg 파일 클릭 했을때도 setOpen 되게끔 변경

--- src/components/common/DropDown/ui/DropDownButton.tsx
+++ src/components/common/DropDown/ui/DropDownButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useDropDownState } from '../module/DropDownStateContext';
 
@@ -6,18 +6,39 @@ import { IconComponents } from '@components/common/Icon/IconComponents';
 
 const DropDownButton = () => {
   const { open, handleOpen, selectedItem } = useDropDownState();
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        buttonRef.current &&
+        !buttonRef.current.contains(e.target as Node) &&
+        svgRef.current &&
+        !svgRef.current.contains(e.target as Node)
+      ) {
+        handleOpen(false);
+      }
+    };
+
+    if (open) document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [open]);
 
   return (
-    <MainButton open={open} onClick={handleOpen}>
+    <MainButton open={open} onClick={() => handleOpen(!open)} ref={buttonRef}>
       {selectedItem}
-      {open ? <IconComponents.down /> : <IconComponents.up />}
+      {open ? <IconComponents.up width={16} height={16} /> : <IconComponents.down width={16} height={16} />}
     </MainButton>
   );
 };
 
 const MainButton = styled.button<{ open: boolean }>`
   display: flex;
-  border: ${({ theme, open }) => ('1.5px solid ' + open ? `${theme.COLORS.prographyRed}` : `${theme.COLORS.grey300}`)};
+  border: 1.5px solid ${({ theme, open }) => (open ? `${theme.COLORS.prographyRed}` : `${theme.COLORS.grey300}`)};
   border-radius: 8px;
   align-items: center;
   justify-content: center;
--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -7,12 +7,13 @@ interface IconProps {
   width?: number;
   height?: number;
   fill?: string;
+  ref?: React.Ref<SVGSVGElement>;
 }
 
-const Icon = ({ name, width = 21, height = 21, fill = '#ffffff' }: IconProps) => {
+const Icon = ({ name, width = 21, height = 21, fill = '#ffffff', ref }: IconProps) => {
   const IconComponent = IconComponents[name];
 
-  return <IconComponent width={width} height={height} fill={fill} />;
+  return <IconComponent width={width} height={height} fill={fill} ref={ref} />;
 };
 
 export default Icon;
