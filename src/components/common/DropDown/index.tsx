70a7d94f86707dd35ec1fe09f72c0108ad6484ea
JangHoonSeok <orirori512@naver.com>
2024-02-02 21:56:44 +0900

    🍱 assets: up, down 아이콘 추가

--- src/components/common/DropDown/index.tsx
+++ src/components/common/DropDown/index.tsx
@@ -3,7 +3,7 @@ import DropDownMain from './ui/DropDownMain';
 import DropDownMenuItem from './ui/DropDownMenuItem';
 import DropDownMenus from './ui/DropDownMenus';
 
-export const DorpDown = Object.assign(DropDownMain, {
+export const DropDown = Object.assign(DropDownMain, {
   Button: DropDownButton,
   Menus: DropDownMenus,
   MenuItem: DropDownMenuItem,
