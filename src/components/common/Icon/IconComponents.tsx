a63a4b6b2b698ab4be8627a0bffead5e1a3938e2
JangHoonSeok <orirori512@naver.com>
2024-02-02 21:57:40 +0900

    ✨ feat: Icon 컴포넌트에 up, down 아이콘 추가

--- src/components/common/Icon/IconComponents.tsx
+++ src/components/common/Icon/IconComponents.tsx
@@ -1,9 +1,13 @@
 import { ReactComponent as plus } from '@assets/Icons/plus.svg';
 import { ReactComponent as cancel } from '@assets/Icons/cancel.svg';
+import { ReactComponent as up } from '@assets/Icons/chevron_up.svg';
+import { ReactComponent as down } from '@assets/Icons/chevron_down.svg';
 
 const IconComponents = {
   plus,
   cancel,
+  up,
+  down,
 };
 
 export { IconComponents };
