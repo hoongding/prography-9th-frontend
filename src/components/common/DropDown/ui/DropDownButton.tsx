9ce81451d418cd7ee637f707ce2efa6b11406ad9
JangHoonSeok <orirori512@naver.com>
2024-02-02 22:12:46 +0900

    ✨ feat: 드롭다운 컴포넌트 메인 버튼 컴포넌트 작업 완료

--- src/components/common/DropDown/ui/DropDownButton.tsx
+++ src/components/common/DropDown/ui/DropDownButton.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useDropDownState } from '../module/DropDownStateContext';
 
 const DropDownButton = () => {
-  return <div>DropDownButton</div>;
+  const { open, handleOpen, selectedItem } = useDropDownState();
+
+  return (
+    <MainButton open={open} onClick={handleOpen}>
+      {selectedItem}
+    </MainButton>
+  );
 };
+
+const MainButton = styled.button<{ open: boolean }>`
+  display: flex;
+  border: ${({ theme, open }) => ('1.5px solid ' + open ? `${theme.COLORS.prographyRed}` : `${theme.COLORS.grey300}`)};
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  padding: 8px 12px;
+  background-color: ${({ theme, open }) => (open ? theme.COLORS.red50 : theme.COLORS.white)};
+`;
 
 export default DropDownButton;
