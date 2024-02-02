d199c9f75d411147e701cf558de5939ea98fef1e
JangHoonSeok <orirori512@naver.com>
2024-02-02 22:27:02 +0900

    ✨ feat: 드롭다운 메뉴 Wrapper 컴포넌트 완성

--- src/components/common/DropDown/ui/DropDownMenus.tsx
+++ src/components/common/DropDown/ui/DropDownMenus.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useDropDownState } from '../module/DropDownStateContext';
 
-const DropDownMenus = () => {
-  return <div>DropDownMenus</div>;
+interface IDropDownMenus {
+  children: React.ReactNode;
+}
+
+const DropDownMenus = ({ children }: IDropDownMenus) => {
+  const { open } = useDropDownState();
+
+  return <MenuContainer open={open}>{children}</MenuContainer>;
 };
+
+const MenuContainer = styled.div<{ open: boolean }>`
+  display: ${({ open }) => (open ? 'flex' : 'none')};
+  position: absolute;
+  z-index: 1;
+  top: 100%;
+  width: 100%;
+  background-color: white;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  border-radius: 8px;
+  padding: 8px;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+`;
 
 export default DropDownMenus;
